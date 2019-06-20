4fb68fd2ecbf175ed153bd9aa49198c50fcbcb3b
Hugo Silva <jhssilva96@gmail.com>
2019-06-20 20:07:20 +0100

    da

--- src/main/java/gui/CartView.java
+++ src/main/java/gui/CartView.java
@@ -6,10 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class CartView extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -37,6 +48,38 @@ public class CartView extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.add(panel, BorderLayout.SOUTH);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		Component horizontalGlue = Box.createHorizontalGlue();
+		panel.add(horizontalGlue);
+		
+		JButton btnRemove = new JButton("Remove");
+		panel.add(btnRemove);
+		
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		panel.add(horizontalStrut);
+		
+		JButton btnBuy = new JButton("Buy");
+		panel.add(btnBuy);
+		
+		JLabel lblTotal = new JLabel("Pre\u00E7o Total:");
+		panel.add(lblTotal);
+		
+		table = new JTable();
+		table.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Card Shop", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+			},
+			new String[] {
+				"ID", "Nome", "Quantidade", "Valor"
+			}
+		));
+		contentPane.add(table, BorderLayout.CENTER);
 	}
 
 }
--- src/main/java/gui/StoreView.java
+++ src/main/java/gui/StoreView.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
 
 public class StoreView extends JFrame {
 
@@ -35,8 +38,23 @@ public class StoreView extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(5, 214, 424, 43);
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.add(panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton btnAdd = new JButton("Add");
+		panel.add(btnAdd);
+		
+		JButton btnCart = new JButton("Cart");
+		panel.add(btnCart);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(5, 11, 419, 182);
+		contentPane.add(scrollPane);
 	}
-
 }
